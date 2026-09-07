d61e57b00e99306a7412efcce6d45d6f6a476982
zynaptic-lead <fnalistboy@gmail.com>
2026-09-07 19:02:25 +0100

    Fix useEffect import in restaurant menu page

--- app/restaurant/menu/page.tsx
+++ app/restaurant/menu/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { 
   Plus, Search, Filter, Edit, Trash2, CheckCircle2, 
   X, Flame, Clock, Sparkles, AlertCircle, Image as ImageIcon,
